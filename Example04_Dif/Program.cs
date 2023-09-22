815c2444f614b4a88a549f1e403ba1734f9fcb6d
Viktor <v.kurochkin89@yandex.ru>
2023-09-22 20:39:17 +0300

    Добавил генерацию двух чисел и их дальнейшее деление

--- Example04_Dif/Program.cs
+++ Example04_Dif/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,10); // 1 2 3 ... 9 - здесь минимальное включает, а максимальное нет, то есть [min,max)
+double numberB = new Random().Next(10,20);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
