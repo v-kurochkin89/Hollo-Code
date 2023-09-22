97096ec403fc4f5eec69aa663e29aa1d076a6061
Viktor <v.kurochkin89@yandex.ru>
2023-09-22 20:59:12 +0300

    Добавил проект с получением остатка от деления рандомных чисел

---
+++ Example05_Random/Program.cs
@@ -0,0 +1,8 @@
+﻿double numberA = new Random().Next(101,1000);
+double numberB = new Random().Next(10,100);
+Console.Write("Первое число: ");
+Console.WriteLine(numberA);
+Console.Write("Второе число: ");
+Console.WriteLine(numberB);
+Console.Write("Остаток от деления первого на второе: ");
+Console.WriteLine(numberA % numberB);
