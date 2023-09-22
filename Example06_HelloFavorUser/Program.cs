068675bc53757734f037be66a5ddd87b41d8e1a0
Viktor <v.kurochkin89@yandex.ru>
2023-09-22 21:17:29 +0300

    Добавил программу с выбором вариантов приветствия

---
+++ Example06_HelloFavorUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "виктор")
+{
+    Console.WriteLine("УРААА! Это же мой любимый пользователь!");
+}
+else
+{
+    Console.Write("Привет, маловажный пользователь ");
+    Console.WriteLine(username);
+}
