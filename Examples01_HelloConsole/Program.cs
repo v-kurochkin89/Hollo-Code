87de9233a42380fcf8e34c1057e70906b35c20ed
Viktor <v.kurochkin89@yandex.ru>
2023-09-24 07:52:42 +0300

    Добавил файл для семинара и для девятого примера

--- Examples01_HelloConsole/Program.cs
+++ Examples01_HelloConsole/Program.cs
@@ -1,3 +1,1 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World! 2");
-Console.WriteLine("Hello, World! 3");
+﻿
---
+++ seminar/Program.cs
@@ -0,0 +1,34 @@
+﻿// a = 5; b = 7 -> Первое число 5 меньше чем второе число 7
+//a = 2; b = 2 -> Введенные числа равны 2
+// a = -3; b = -9 -> Первое число -3 больше чем второе число -9
+
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber == secondNumber)
+//     Console.WriteLine("Введенные числа равны " + firstNumber);
+// else if (firstNumber < secondNumber)
+//     Console.WriteLine("Первое число " + 'firstNumber' + " меньше чем второе число " + secondNumber);
+// else 
+//     Console.WriteLine("Первое число " + firstNumber + " больше чем второе число " + secondNumber);
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+if (b > a);
+max = b;
+    else
+        if (c > a);
+        max = c;
+        
+Console.WriteLine(max)
